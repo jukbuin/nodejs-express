eb79ce58ee7c99bd334964ca1dafa2bcab72b3c9
jukbuin <h04095@naver.com>
2024-04-17 23:42:18 +0900

    2024/04/17 라우팅 마무리

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const template = require("../lib/template");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(req.list);
+  var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}
+               <img src="/images/hello.jpg" alt="hello" style="width:300px; display: block; margin-top: 10px;">
+               `,
+      `<a href="/topic/create">create</a>`
+  );
+  res.send(html);
 });
 
 module.exports = router;
